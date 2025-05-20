0ca54e97df8a8d74ccebe929a1d96f69cb69802e
Vicky Kumar <hivickykumar96@gmail.com>
2025-05-20 14:04:21 +0530

    heanding change test

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vicky + Rohit</h1>
+      <h1>Vicky + Rohit 2.0</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
