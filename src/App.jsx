9166bc3721170ccabd0fa452514db0cf02451f55
sanjeet sharma <1sanjeetsharma@gmail.com>
2025-05-25 00:07:47 +0530

    MovieCard component created and styled

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@
 import './App.css'
 import Footer from './components/layout/Footer'
 import Header from './components/layout/Header'
+import MovieCard from './components/layout/MovieCard'
 function App() {
-
   return (
     <>
       <Header/>
+       <MovieCard/>
       <Footer/>
     </>
   )
