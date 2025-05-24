03b2162ade6da2ce945b0c66ed169dfcc702090b
sanjeet sharma <1sanjeetsharma@gmail.com>
2025-05-24 20:38:02 +0530

    fixed header compnent not found issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import Footer from './components/layout/Footer'
-import Header from './components/layout/header'
+import Header from './components/layout/Header'
 function App() {
 
   return (
